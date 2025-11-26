8b18e71ae64d03faf85245e7c7b7966917b9ac5d
arthurllucas <arthurlucas0910@gmail.com>
2025-11-26 10:48:50 -0300

    Atualizando

--- src/persistencia/MovimentacaoDAO.java
+++ src/persistencia/MovimentacaoDAO.java
@@ -1,48 +1,35 @@
-
 package persistencia;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
-<<<<<<< HEAD
 import java.util.ArrayList;
 import modelos.classes.Movimentacao;
 import modelos.interfaces.IMovimentacaoCRUD;
 
 public class MovimentacaoDAO implements IMovimentacaoCRUD {
-=======
-import modelos.classes.Movimentacao;
-import modelos.interfaces.MovimentacaoCRUD;
 
-public class MovimentacaoDAO implements MovimentacaoCRUD {
->>>>>>> branch_arthur_lucas
-  private String nomeDoArquivoNoDisco = null;
+    private String nomeDoArquivoNoDisco = null;
 
-  public MovimentacaoDAO(){
-    nomeDoArquivoNoDisco = "./src/bancodedados/Movimentacoes.txt";
-  }
-  @Override
-  public void salvar(Movimentacao movimentacao) throws Exception {
-    try{
-      BufferedWriter bw =new BufferedWriter(new FileWriter(nomeDoArquivoNoDisco,true));
-      //Escreve no arquivo
-      String str = movimentacao.getIdMovimentacao() + ";";
-      str += movimentacao.getIdVeiculo() + ";";
-<<<<<<< HEAD
-      str += movimentacao.getDescricao() + ";";
-      str += movimentacao.getDescricao() + "\n";
-=======
-      str += movimentacao.getDescrição() + 
-      str += tipoDeDespesa.getDescricao() + "\n";
->>>>>>> branch_arthur_lucas
-      bw.write(str);
-      //fecha o arquivo
-      bw.close();		
-    }catch(Exception erro){
-      String msg = "Persistencia - Metodo Salvar - "+erro.getMessage();
-      throw new Exception(msg);
+    public MovimentacaoDAO() {
+        nomeDoArquivoNoDisco = "./src/bancodedados/Movimentacoes.txt";
     }
-  }
-<<<<<<< HEAD
+
+    @Override
+    public void salvar(Movimentacao movimentacao) throws Exception {
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(nomeDoArquivoNoDisco, true));
+            //Escreve no arquivo
+            String str = movimentacao.getIdMovimentacao() + ";";
+            str += movimentacao.getIdVeiculo() + ";";
+            str += movimentacao.getDescricao() + "\n";
+            bw.write(str);
+            //fecha o arquivo
+            bw.close();
+        } catch (Exception erro) {
+            String msg = "Persistencia - Metodo Salvar - " + erro.getMessage();
+            throw new Exception(msg);
+        }
+    }
 
     @Override
     public ArrayList<Movimentacao> listaDeMovimentacoes() throws Exception {
@@ -63,6 +50,5 @@ public class MovimentacaoDAO implements MovimentacaoCRUD {
     public void remover(int idMovimentacao) throws Exception {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-=======
->>>>>>> branch_arthur_lucas
+
 }
