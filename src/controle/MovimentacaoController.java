af9d5b59594f433a0053e4f467074c190217932c
arthurllucas <arthurlucas0910@gmail.com>
2025-11-28 15:40:47 -0300

    Commit do dia 28/11, no qual eu arrumei alguns componentes da tela e organizei o controller

--- src/controle/MovimentacaoController.java
+++ src/controle/MovimentacaoController.java
@@ -4,6 +4,7 @@ package controle;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Date;
 import modelos.classes.Movimentacao;
 import persistencia.MovimentacaoDAO;
@@ -15,23 +16,26 @@ public class MovimentacaoController {
         movimentacaoDAO = new MovimentacaoDAO();
     }
     
-    
-    
     public void registrarMovimentacao(int idVeiculo, int idTipoDespesa, String descricao, String dataMovimentacao, double valor) throws Exception{
         try {
             if (idVeiculo <= 0) throw new Exception ("ID do veículo inválido!");
             if (idTipoDespesa <= 0) throw new Exception ("ID da despesa inválida!");
             if (descricao ==  null || descricao.trim().equals("")) throw new Exception ("Descrição vazia!");
             if (eDataValida(dataMovimentacao) == false) throw new Exception ("Data inválida!");
-            if (valor < 0) throw new Exception ("Valor inválido!");
+            if (valor <= 0) throw new Exception ("Valor inválido!");
             int idMovimentacao = movimentacaoDAO.gerarId();
             SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
             Date dataConvertida = sdf.parse(dataMovimentacao);
             Movimentacao movimentacao = new Movimentacao(idMovimentacao, idVeiculo, idTipoDespesa, descricao, dataConvertida, valor);
             movimentacaoDAO.salvar(movimentacao);
+            
         } catch (Exception erro) {
-            erro.getMessage();
+            throw erro;
         }
+    }
+    
+    public ArrayList<Movimentacao> listarMovimentacoes() throws Exception {
+        return movimentacaoDAO.listaDeMovimentacoes();
     }
     
     public boolean eDataValida(String dataMovimentacao ) {
